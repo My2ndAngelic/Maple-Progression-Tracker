1a6fb31c65401d74517ae621530b615c781cebb1
My2ndAngelic <m2a@my2ndangelic.com>
2025-05-19 10:39:18 +0200

    Revert "Lorem ipsum on about page"
    This reverts commit 0a5122f93f740ca418b7942e399a3a0b5e699bc0.

--- script/about.js
+++
@@ -1,7 +0,0 @@
-// About page initialization
-import { initializeUI } from './ui.js';
-
-// Initialize the UI when the page loads
-document.addEventListener('DOMContentLoaded', () => {
-    initializeUI();
-});
